7f9dee59e0e66842a286d84e87fc64c0ca065286
Bob Mottram <bob.mottram@codethink.co.uk>
2016-07-28 16:10:18 +0100

    system test for a case where the current checkouts are not the same as the head of master

--- tests/system_tests.c
+++ tests/system_tests.c
@@ -25,7 +25,9 @@ void test_system_create_from_repos()
 	sc_system sys;
 	int retval, p;
 	char * repo_url1 = "https://github.com/CodethinkLabs/frepo";
+	char * checkout1 = "618e913171d2cf8e731bc0c4ab4516f395dddba0";
 	char * repo_url2 = "https://github.com/CodethinkLabs/firehose";
+	char * checkout2 = "c2bf2f9961bea6c382fe17bbf1057a58ce6a3d2b";
 	char template[] = "/tmp/scalam.XXXXXX";
 	char * repo_dir;
 	char commandstr[SC_MAX_STRING];
@@ -38,7 +40,16 @@ void test_system_create_from_repos()
 	/* clone repos into a test directory */
 	sprintf(commandstr,"git clone %s %s/frepo",repo_url1, repo_dir);
 	run_shell_command(commandstr);
+	/* do a checkout so that we're not at the HEAD of master */
+	sprintf(commandstr,"cd %s/frepo\ngit checkout %s -b %s",
+			repo_dir,checkout1,checkout1);
+	run_shell_command(commandstr);
+
 	sprintf(commandstr,"git clone %s %s/firehose",repo_url2, repo_dir);
+	run_shell_command(commandstr);
+	/* do a checkout so that we're not at the HEAD of master */
+	sprintf(commandstr,"cd %s/firehose\ngit checkout %s -b %s",
+			repo_dir,checkout2,checkout2);
 	run_shell_command(commandstr);
 
 	retval = system_create_from_repos(&sys, repo_dir);
@@ -56,9 +67,27 @@ void test_system_create_from_repos()
 
 	/* check that programs were actually created */
 	for (p = 0; p < sys.no_of_programs; p++) {
+		/* has a name */
 		assert(sys.program[p].name[0] != 0);
+
+		/* has some commits */
 		assert(sys.program[p].no_of_versions > 0);
+
+		/* has a versions.txt file */
 		assert(sys.program[p].versions_file[0] != 0);
+
+		/* non-zero current commit index */
+		assert(sys.program[p].version_index > 0);
+
+		/* check that we are not at HEAD of master */
+		if (sys.program[p].version_index == sys.program[p].no_of_versions) {
+			printf("\nversion_index is the same as the head of master\n");
+			sprintf(commandstr,"rm -rf %s", repo_dir);
+			run_shell_command(commandstr);
+		}
+		assert(sys.program[p].version_index < sys.program[p].no_of_versions);
+
+		/* check that versions.txt file exists */
 		assert(file_exists(sys.program[p].versions_file));
 	}
 
