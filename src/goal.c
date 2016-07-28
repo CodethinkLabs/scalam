1d4c2b0dfa7d1a91d86e55db4462a1a331500b1c
Bob Mottram <bob.mottram@codethink.co.uk>
2016-07-28 13:56:34 +0100

    Goal placeholder

---
+++ src/goal.c
@@ -0,0 +1,35 @@
+/*
+	Smart search for upgrade paths
+	Copyright (C) 2016 Andrew Leeming <andrew.leeming@codethink.co.uk> and
+					   Bob Mottram <bob.mottram@codethink.co.uk>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "scalam.h"
+
+/**
+ * @brief Creates a goal to try to get to the latest versions
+ * @param sys System object
+ * @param goal Returned goal object
+ * @returns zero on success
+ */
+int goal_create_latest_versions(sc_system * sys, sc_goal * goal)
+{
+	memset((void*)goal, '\0', sizeof(sc_goal));
+
+	/* TODO */
+
+	return 0;
+}
