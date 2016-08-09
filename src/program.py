1fd42042edce953e4cb16e3e3ce53506453a7e1b
Bob Mottram <bob.mottram@codethink.co.uk>
2016-08-09 15:42:40 +0100

    Convert comments to doxygen style and simplify some method names

--- src/program.py
+++ src/program.py
@@ -30,195 +30,159 @@ except ImportError:
 
 class Program:
     def __init__(self, repo, name=None, installed=False):
-        '''
-        @param name (String) Name of the program
-        @param installed (bool) If the program is installed or not
-        '''
-        
-        ##Type checking params
+        ## Constructor
+        # @param name (String) Name of the program
+        # @param installed (bool) If the program is installed or not
+
+        ## Type checking params
         if not isinstance(repo, AbsRepoType):
             raise TypeError(u"Program 'repo' expects a repo type e.g. GitType")
-        
+
         # TODO name could be obmitted and recovered from directory/repo?
         if not isinstance(name,(str,unicode)):
             raise TypeError(u"Program 'name' expects a string")
 
         if not isinstance(installed,bool):
-            raise TypeError(u"Program 'installed' expects a bool")       
+            raise TypeError(u"Program 'installed' expects a bool")
 
         self.repo_ref=repo
         self.name=unify(name)
         self.directory=directory
         self.installed=installed
-    
-    
-    
+
     def getCurrentHead(self):
-        '''
-        Returns the latest version or commit for the software
-        '''
-        
+        ## Returns the latest version or commit for the software
+
         return self.repo_ref.getHead()
-    
+
     def getName(self):
-        '''
-        Gets the name of the repo/program
-        '''
-        
+        ## Gets the name of the repo/program
+
         return self.name
-    
-    
-    
-    
+
     ##
     # TODO - Includes renaming/re-factoring into current code
     ##
-    
-    def getVersionFromChangelog(self):
-        '''
-        * @brief Gets a list of versions from a changelog
-        * @param changelog_filename Filename of the changelog
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_changelog()
-        '''
+
+    def FromChangelog(self):
+        ## Get version numbers from a changelog
+        # @brief Gets a list of versions from a changelog
+        # @param changelog_filename Filename of the changelog
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
-    def getVersionFromTarball(self):
-        '''
-        * @brief Gets a list of versions from a changelog within a tarball
-        * @param repos_dir Directory where the git repo will be checked out
-        * @param tarball_url URL of the tarball
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_tarball()
-        '''
+
+    def FromTarball(self):
+        ### Get version numbers from a tarball
+        # @brief Gets a list of versions from a changelog within a tarball
+        # @param repos_dir Directory where the git repo will be checked out
+        # @param tarball_url URL of the tarball
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
-    def getVersionFromDeb(self):
-        '''
-        * @brief Gets a list of versions from a debian package
-        * @param repos_dir Directory where the git repo will be checked out
-        * @param deb_url URL where the debian package can be downloaded from
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_deb_package()
-        '''
+
+    def FromDeb(self):
+        ## Get version numbers from a deb package
+        # @brief Gets a list of versions from a debian package
+        # @param repos_dir Directory where the git repo will be checked out
+        # @param deb_url URL where the debian package can be downloaded from
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
-    def getVersionFromRpm(self):
-        '''
-        * @brief Gets a list of versions from a RPM package
-        * @param repos_dir Directory where the git repo will be checked out
-        * @param rpm_url URL where the debian package can be downloaded from
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_rpm_package(
-        '''
+
+    def FromRpm(self):
+        ## Get version numbers from an RPM package
+        # @brief Gets a list of versions from a RPM package
+        # @param repos_dir Directory where the git repo will be checked out
+        # @param rpm_url URL where the debian package can be downloaded from
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
-    def getVersionFromRepo(self):
-        '''
-        * @brief Gets a list of versions from a repo as a file valled versions.txt
-        * @param repos_dir Directory where the git repo will be checked out
-        * @param repo_url URL of the git repo or tarball
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_repo(
-        '''
+
+    def FromRepo(self):
+        ## Get version numbers from a git repo
+        # @brief Gets a list of versions from a repo as a file valled versions.txt
+        # @param repos_dir Directory where the git repo will be checked out
+        # @param repo_url URL of the git repo or tarball
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
-    def getVersionFromAptitude(self):
-        '''
-        * @brief Gets a list of versions provided by aptitude
-        * @param prog Program object
-        * @returns zero on success
-        */
-        int program_get_versions_from_aptitude()
-        '''
+
+    def FromAptitude(self):
+        ## Gets version numbers using aptitude command
+        # @brief Gets a list of versions provided by aptitude
+        # @param prog Program object
+        # @returns zero on success
         pass
-    
+
     def getVersions(self):
-        '''
-        Gets a list of version/commits from a repo
-        '''
+        ## Returns a list of version numbers or commits
         return self.git_ref.getVersions()
-    
-    
+
+
     @staticmethod
     def isValidName(name):
-        '''
-        Checks whether the name of the given program is valid
-        @return bool
-        '''
- 
+        ## Checks whether the name of the given program is valid
+        # @return bool
+
         if not isinstance(name,(str,unicode)):
             raise TypeError(u"Program.isValidName 'name' expects a string")
-        
+
         # TODO do we only care if the first char is a letter?
         if not name[0].isalpha():
             return False
-        
+
         return True
 
 class AbsRepoType:
     def assertValidDirectory(self, directory):
         if not isinstance(directory,(str,unicode)):
             raise TypeError(u"%s 'directory' expects a string"%self.__class__.__name__)
-        
+
         #TODO check if exists, fail or create? currently fails
         if not os.path.exists(directory):
             raise IOError(u"%s 'path' given does not exist"%self.__class__.__name__)
-    
+
 
     def getVersions(self):
         raise NotImplementedError
-    
+
 
     def getHead(self):
         raise NotImplementedError
-    
-    
+
+
 class DirType(AbsRepoType):
     pass
 class GitType(AbsRepoType):
     def __init__(self, url, clone_path):
-        
+
         if not isinstance(url,(str,unicode)):
-            raise TypeError(u"GitType 'url' expects a string")  
-        
+            raise TypeError(u"GitType 'url' expects a string")
+
         self.assertValidDirectory(clone_path)
-        
+
         self.url=url
         self.path=clone_path
-        
+
         #Do the git clone
         self.git_ref=git.Repo.clone_from(url=url, to_path=clone_path)
-        
+
     def getVersions(self):
-        '''
-        Grab all of the version shas of this git repo
-        
-        @return List of versions
-        '''
-        
+        ## Grab all of the version shas of this git repo
+        # @return List of versions
+
         versions=[]
+
         #Grab all of the shas from this repo
         for commits in self.git_ref.iter_commits():
             versions.append(commits.hexsha)
-        
+
         return versions
-    
+
     def getHead(self):
-        '''
-        Grabs the commit sha of HEAD
-        '''
-        
+        ## Grabs the commit sha of HEAD
+        # @return sha of HEAD
         return self.git_ref.head.ref.commit.hexsha
 
 class TarType(AbsRepoType):
