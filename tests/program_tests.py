50738ed84b777bfaae724ddd8f286799e8a39f55
Bob Mottram <bob.mottram@codethink.co.uk>
2016-08-10 16:54:05 +0100

    Unit test for getting commits from a git repo

--- tests/program_tests.py
+++ tests/program_tests.py
@@ -66,7 +66,30 @@ class TestProgram(unittest.TestCase):
         self.assertTrue(program.getCurrentHead() == expected_head)
 
     def test_versions_from_git(self):
-        self.assertTrue(True)
+        expected_head="5b2a7bffd0a7f564f55977a72429f36d0cdc9eaa"
+        repo_dir=tempfile.mkdtemp('.scalam')
+        repo_url="https://github.com/CodethinkLabs/frepo"
+        repo_name="frepo"
+
+        # Create the git instance and clone
+        gtype=GitType(repo_url,repo_dir)
+
+        # Missing name param
+        try:
+            program=Program(repo=gtype)
+        except TypeError:
+            self.assertTrue(False)
+
+        program=Program(repo=gtype,name=repo_name)
+
+        # checkout to the expected head
+        program.checkout(expected_head)
+
+        versions = program.getVersions()
+        self.assertTrue(versions[0] == u"5b2a7bffd0a7f564f55977a72429f36d0cdc9eaa")
+        self.assertTrue(versions[1] == u"618e913171d2cf8e731bc0c4ab4516f395dddba0")
+        self.assertTrue(versions[2] == u"7e251622aaf6bfb85fad018985671c185ceccdd8")
+        self.assertTrue(versions[3] == u"a02b369a75fbb20bb79908cbb64447538c2fedec")
 
     def test_valid_name(self):
         self.assertTrue(True)
